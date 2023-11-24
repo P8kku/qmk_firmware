fb30f45c848dadbe67aadc7d9cd2d62218f783d9
Pakku <danill3894@gmail.com>
2023-11-24 00:22:18 -0500

    updating binds

--- keyboards/boardsource/lulu/keymaps/P8kku/keymap.c
+++ keyboards/boardsource/lulu/keymaps/P8kku/keymap.c
@@ -1,6 +1,6 @@
  // Copyright 2022 Cole Smith <cole@boadsource.xyz>
 // SPDX-License-Identifier: GPL-2.0-or-later
-// custom keymap /u/P8kku 
+// custom keymap /u/P8kku
 // First keymap I've defined using QMK's firmware docs, previously daily'd keyboard was a Keychron Q8
 // so you'll probably see alot of influence from that layout here :')
 
@@ -18,7 +18,7 @@
  [ KC_RBRC
  ] KC_LBRC
  \ KC_BACKSLASH
- 
+
 */
 
 #include QMK_KEYBOARD_H
@@ -48,7 +48,7 @@ enum custom_keycodes {
 //#define ZOOM_IN KC_MS_WH_UP(KC_LEFT_CTRL) //QWERTY KEYMAP ENCODER DEF
 //#define ZOOM_OUT KC_MS_WH_DOWN(KC_LEFT_CTRL)
 #define LW_ZOOM_IN LCTL(KC_MINUS) //LOWER MOD KEYMAP ENCODER DEF
-#define LW_ZOOM_OUT LCTL(KC_EQUAL) 
+#define LW_ZOOM_OUT LCTL(KC_EQUAL)
 
 /* TODO potentially use combo functionality to accomplish this?
 
@@ -57,23 +57,28 @@ make: ctrl+enter='HOME'
 make: ctrl+shift+enter='END'
 make: ctrl+9='['
 make: ctrl+0=']'
-make: ctrl+home=end
 make: ctrl+/= '\'
-
 
 */
 
+const uint16_t PROMMEM S_DELETE[] = {KC_LCTL, KC_BSPC, COMBO_END};
+const uint16_t PROMMEM S_HOME[] = {KC_LCTL, KC_ENT, COMBO_END};
+const uint16_t PROMMEM S_END[] = {KC_LCTL, KC_LSFT, KC_BSPC, COMBO_END};
+const uint16_t PROMMEM S_LBRC[] = {KC_LCTL, KC_9, COMBO_END};
+const uint16_t PROMMEM S_RBRC[] = {KC_LCTL, KC_8, COMBO_END};
+const uint16_t PROMMEM S_BACKSLASH[] = {KC_LCTL, KC_SLSH, COMBO_END};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- 
+
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BKSPC|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Enter|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|Mute   |   |Play    |------+------+------+------+------+------|
- * |LCTRL|   Z  |   X  |   C  |   V  |   B  |--------|   |--------|   B  |   N  |   M  |   ,  |   .  |   /  |
+ * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|Mute    |   |Play   |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |--------|   |-------|   B  |   N  |   M  |   ,  |   .  |   /  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LGUI | LAlt |LOWER | /Space  /       \Space \  |RAISE | FN   | RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -83,10 +88,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   QK_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-  KC_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_AUDIO_MUTE,  KC_MEDIA_PLAY_PAUSE,  KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+  KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_AUDIO_MUTE,  KC_MEDIA_PLAY_PAUSE,  KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
                            KC_LGUI, KC_LALT,    LOWER, KC_SPC, KC_SPC, RAISE, MO(_FL), KC_RGUI
 ),
+
+/* _FL
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  ~`  |   F1 |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  F12 |      |  DEL |BKLTSP|      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |PRSCR | SCRLK|KCPAUS|      |      |      |-------.    ,-------|      |      |      |      |      |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /PG UP  /       \PG DN \  | HOME |  FN  | END |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+  [_FL] = LAYOUT(
+    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,           KC_F7,  KC_F8,  KC_F9,   KC_F10,   KC_F11,
+    KC_F12,  _______, KC_DEL, BL_STEP,_______,_______,         _______, _______, _______, _______, _______, _______,
+    KC_PSCR, KC_SCRL, KC_PAUS, _______, _______, _______,      _______, _______, _______, _______, _______, _______,
+    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+                           _______, _______, _______, KC_PGUP,  KC_PGDN , KC_HOME, MO(_FL), KC_END
+),
+
+
+
+
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      | DEL  |
@@ -114,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |  UP  |   0  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|      | [    | LEFT  | Down | Right|      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|      | [    | LEFT  | Down | Right|   ew  |
  * |------+------+------+------+------+------|RGB_TOG|    |    ]  |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |  |      | |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -130,28 +161,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   RGB_TOG, _______,  KC_PLUS, KC_MINS, KC_EQL,  _______, _______, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
 ),
-/* _FL
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  ~`  |   F1 |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F12 |      |  DEL |BKLTSP|      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |PRSCR | SCRLK|KCPAUS|      |      |      |-------.    ,-------|      |      |      |      |      |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /PG UP  /       \PG DN \  | HOME |  FN  | END |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-  [_FL] = LAYOUT(
-    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,           KC_F7,  KC_F8,  KC_F9,   KC_F10,   KC_F11,
-    KC_F12,  _______, KC_DEL, BL_STEP,_______,_______,         _______, _______, _______, _______, _______, _______,
-    KC_PSCR, KC_SCRL, KC_PAUS, _______, _______, _______,      _______, _______, _______, _______, _______, _______,
-    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-                           _______, _______, _______, KC_PGUP,  KC_PGDN , KC_HOME, MO(_FL), KC_END
-),
-
 };
 
 #ifdef ENCODER_MAP_ENABLE
